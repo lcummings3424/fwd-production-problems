04b54dd5bca487a293b98bd4ed14a0b2e9cc8a28
lcummings3424 <lcummings1@hawk.iit.edu>
2021-11-21 23:33:32 -0600

    Finish Step 3.

--- pp-04/site.js
+++ pp-04/site.js
@@ -16,9 +16,9 @@ function double(num) {
 //double(6);
 //console.log('The value of x is', x, '-- it should be 5.');
 
-
-
 /*
+
+
   Step 2. Rewrite the JavaScript `double()` function above so that
   it does not pollute the global namespace: that is, neither its
   variables nor the `double()` function itself can be accessed
@@ -27,10 +27,10 @@ function double(num) {
 
   Be sure to comment out the original `double()` function so you
   can accurately test your work.
-*/
+
 let x = 5;
 
-const double = function(num) {
+let double = function(num) {
 let x = num * 2;
 return x;
 }
@@ -41,8 +41,20 @@ console.log('The value of x is', x, '-- it should be 5.');
 
 
 
-/*
+
   Step 3. Rewrite your corrected `double()` function from Step 2.
   so that non-number values passed into the function are handled
   in some reasonable way.
 */
+  let x = 5;
+
+  let double = function(num) {
+    if(typeof(num) != "number"){
+      console.log("Your input was invalid. Please enter a number.")
+      return;
+    }
+  let x = num * 2;
+  return x;
+  }
+  double(6);
+  console.log('The value of x is', x, '-- it should be 5.');
